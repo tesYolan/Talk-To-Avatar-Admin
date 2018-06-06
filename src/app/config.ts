61335edc81d5a827a79e793309c4bce49fc55852
Tesfa Yohannes <tesfa1993@yahoo.com>
2018-06-06 13:05:53 +0300

    Set to localhost for now

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,8 @@
 export class ServerConfiguration {
-  readonly backend_url= '192.168.1.48';
+  readonly backend_url= 'localhost';
   readonly backend_port= '3011';
-  readonly frontend_url= '192.168.1.48';
+  readonly frontend_url= 'localhost';
   readonly frontend_port= '3002';
-  readonly stream_url = '192.168.1.48';
+  readonly stream_url = 'localhost';
   readonly stream_port = '5443';
 }
